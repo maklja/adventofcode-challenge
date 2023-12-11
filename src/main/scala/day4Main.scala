6a77c1b002fa94f471a55ab78abc1ceb13b36654
Bojan Makljenovic <maklja90@gmail.com>
2023-12-11 23:59:26 +0100

    Day4 - second part solution

--- src/main/scala/day4Main.scala
+++ src/main/scala/day4Main.scala
@@ -1,7 +1,7 @@
 import scala.io.Source
 import scala.util.Using
 
-case class Card(id: String, winningCardNumbers: Seq[Int], cardNumbers: Seq[Int])
+case class Card(id: Int, winningCardNumbers: Seq[Int], cardNumbers: Seq[Int])
 
 def parseCardData(cardData: String) = {
     val trimmedCardData = cardData.trim.replaceAll(" +", " ")
@@ -10,30 +10,31 @@ def parseCardData(cardData: String) = {
     val winningCardNumbersSeq = winningCardNumbers.split(" ").map( _.toInt ).toSeq
     val cardNumbersSeq = cardNumbers.split(" ").map( _.toInt ).toSeq
 
-    Card(cardId, winningCardNumbersSeq, cardNumbersSeq)
+    Card(cardId.toInt, winningCardNumbersSeq, cardNumbersSeq)
 }
 
-def processCardWinningNumbers(cardsData: Seq[String]) = {
+def processCardWinningNumbers(cardsData: Seq[String]) = 
     cardsData.map(cardData => {
         val card = parseCardData(cardData)
         val winMatch = card.cardNumbers.intersect(card.winningCardNumbers).length
-        if (winMatch > 1) {
-            val points = Range.inclusive(1, winMatch - 1)
-            points.fold(1)((sum, i) => sum * 2)
-        } else if (winMatch == 1) {
-            1
-        } else {
-            0
-        }
+        (card, winMatch)
+    }).foldLeft(Map[Int, Int]())((map, cardResults) => {
+        val cardsRange = Range.inclusive(cardResults._1.id + 1, cardResults._1.id + cardResults._2)
+        val cardCount = map.get(cardResults._1.id).getOrElse(0) + 1
+        cardsRange.foldLeft(map)((map, cardId) => 
+            map.updatedWith(cardId)(curValue => curValue match {
+                case Some(cardNum) => Some(cardNum + cardCount)
+                case None => Some(cardCount)
+            })
+        ) + (cardResults._1.id -> cardCount)
     })
-}
 
 @main def day4Solution: Unit = {
     Using.Manager { use =>
 
         val engineSchema = use(Source.fromResource("day4/input.txt")).getLines().toSeq
-        val totalPoints = processCardWinningNumbers(engineSchema).sum
+        val totalCards = processCardWinningNumbers(engineSchema).values.sum
 
-         println(f"Total points sum: ${totalPoints}")
+         println(f"Total cards sum: ${totalCards}")
     }
 }
