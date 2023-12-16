88b0feef750cb1c27a244aba420cc7bf313053a9
Bojan Makljenovic <maklja90@gmail.com>
2023-12-16 22:52:11 +0100

    Day5 - second part solution optimized

--- src/main/scala/day5Main.scala
+++ src/main/scala/day5Main.scala
@@ -2,24 +2,41 @@ import scala.io.Source
 import scala.util.Using
 
 case class MapRange(destinationStart: Long, sourceStart: Long, length: Long) {
+    val destinationEnd = destinationStart + length
+
     def canMapValue(value: Long) = value >= sourceStart && value <= sourceStart + length - 1
 
-    def mapValue(value: Long): Long = {
-        destinationStart + value - sourceStart
+    def mapValue(value: Long): (Long, Long) = {
+        val destinationValue = destinationStart + value - sourceStart
+        (destinationValue, destinationStart + length - destinationValue)
     }
 }
 
 case class MapRanges(id: String, ranges: Seq[MapRange]) {
-    def mapValue(value: Long): Option[Long] = 
-        ranges.find( _.canMapValue(value) ).map(range => range.mapValue(value))
+    def apply(id: String, ranges: Seq[MapRange]): MapRanges =
+        new MapRanges(id, ranges.sortBy( _.destinationStart ))
+
+    def findClosestDestinationRange(destinationValue: Long) = 
+        ranges.find(r => destinationValue < r.destinationEnd) match {
+            case Some(closestDestValue) => closestDestValue.destinationEnd
+            case _ => Long.MaxValue
+        }
+
+    def mapValue(value: Long): (Long, Long) = 
+        ranges.find( _.canMapValue(value) )
+                .map(range => range.mapValue(value)) match {
+                    case Some(mapRangeValues) => mapRangeValues
+                    case _ => (value, findClosestDestinationRange(value))
+                }
 }
 
 class MapNode(private val mapRanges: MapRanges, private val nextNode: MapNode) {
 
-    def mapValue(value: Long): Long = {
-        val mappedValue = mapRanges.mapValue(value).getOrElse(value)
+    def mapValue(value: Long): (Long, Long) = {
+        val mappedValue = mapRanges.mapValue(value)
         if (nextNode != null) {
-            nextNode.mapValue(mappedValue)
+            val nextMappedValue = nextNode.mapValue(mappedValue._1)
+            (nextMappedValue._1, Math.min(nextMappedValue._2, mappedValue._2))
         } else {
             mappedValue
         }
@@ -70,10 +87,19 @@ def createMappingChain(mapRanges: Seq[MapRanges]): MapNode = {
 
 def minLocation(startSeed: Long, seedRange: Long, mapNode: MapNode) = {
     println(f"Seed ${startSeed}, ${seedRange}")
+    
     var min: Long = Long.MaxValue
-    for (i <- startSeed to startSeed + seedRange) {
-        min = Math.min(min, mapNode.mapValue(i))
+    val range = Range.Long(startSeed, startSeed + seedRange, 1).toIterator
+    while (range.hasNext) {
+        val seed = range.next();
+        val mappedValue = mapNode.mapValue(seed)
+        min = Math.min(min, mappedValue._1)
+        var toSkip = mappedValue._2 - 1
+        while(toSkip > 0 && range.hasNext) 
+            range.next()
+            toSkip -= 1
     }
+
     min
 }
 
@@ -89,7 +115,7 @@ def processSeedsLocations(listOfMaps: Seq[String]): Long = {
 @main def day5Solution: Unit = {
     Using.Manager { use =>
 
-        val listOfMaps = use(Source.fromResource("day5/smallInput.txt")).getLines().toSeq
+        val listOfMaps = use(Source.fromResource("day5/input.txt")).getLines().toSeq
         val closestLocation = processSeedsLocations(listOfMaps)
 
         println(f"Closest seed location: ${closestLocation}")
