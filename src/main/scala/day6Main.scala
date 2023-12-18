e3cbb8b6ea04ed40468f3d50ed4dcad9a68a9c9a
Bojan Makljenovic <maklja90@gmail.com>
2023-12-18 08:00:36 +0100

    Day6 - second part solution

--- src/main/scala/day6Main.scala
+++ src/main/scala/day6Main.scala
@@ -1,35 +1,35 @@
 import scala.io.Source
 import scala.util.Using
 
-case class RaceResult(time: Int, distance: Int)
+case class RaceResult(time: Long, distance: Long)
 
 def calcHoldButtonTime(bestRaceResult: RaceResult, winDistanceAmount: Int = 1) = {
     // equation (time - i) * i = distance => i^2 - time * i + distance
-    val b = bestRaceResult.time 
+    // i = (-b +/- sqrt(b^2 - 4*c)) / 2 
+    val b = bestRaceResult.time
     val c = bestRaceResult.distance + winDistanceAmount
 
     val part = Math.sqrt(b * b - 4 * c)
-    val rangeMin = Math.ceil((b - part) / 2).asInstanceOf[Int]
-    val rangeMax = Math.floor((b + part) / 2).asInstanceOf[Int]
+    val rangeMin = Math.ceil((b - part) / 2).asInstanceOf[Long]
+    val rangeMax = Math.floor((b + part) / 2).asInstanceOf[Long]
 
-    Range.Inclusive(rangeMin, rangeMax, 1)
+    Range.Long.inclusive(rangeMin, rangeMax, 1)
 } 
 
 def parseRaces(races: Seq[String]) = {
     val raceData = races.map(race => race.trim.replaceAll(" +", " "))
-    val timeData = raceData(0).replace("Time:", "").trim().split(" ").map( _.toInt ).toSeq
-    val distanceData = raceData(1).replace("Distance:", "").trim().split(" ").map( _.toInt ).toSeq
+    val timeData = raceData(0).replace("Time:", "").replaceAll(" +", "").toLong
+    val distanceData = raceData(1).replace("Distance:", "").replaceAll(" +", "").toLong
     
-    timeData.zipWithIndex.map((indexedTime) => RaceResult(indexedTime._1, distanceData(indexedTime._2)))
+    RaceResult(timeData, distanceData)
 }
 
 @main def day6Solution: Unit = {
     Using.Manager { use =>
 
         val raceTimes = use(Source.fromResource("day6/input.txt")).getLines().toSeq
-        val variationsCount = parseRaces(raceTimes).map(raceResult => 
-            calcHoldButtonTime(raceResult).length
-        ).reduce((holdTime1, holdTime2) => holdTime1 * holdTime2)
+        val raceResult = parseRaces(raceTimes)
+        val variationsCount = calcHoldButtonTime(raceResult).length
 
         println(f"Races variations count: ${variationsCount}")
     }
