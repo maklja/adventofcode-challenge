7426099215380b1a19c51ec0293a27f66acf909a
Bojan Makljenovic <maklja90@gmail.com>
2023-12-26 10:40:36 +0100

    Format day1 challenge

--- src/main/scala/day1Main.scala
+++ src/main/scala/day1Main.scala
@@ -1,45 +1,50 @@
 import scala.io.Source
 import scala.util.Using
 
-case class NumberPointer(name: String, value: Int, index: Int)
+object Day1Challenge:
 
-val numbers: Seq[String] = Seq("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+  case class NumberPointer(name: String, value: Int, index: Int)
 
-def joinFirstAndLastDigit(str: String) = {
+  val numbers: Seq[String] = Seq("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+  def joinFirstAndLastDigit(str: String): Int =
     val firstDigit = str.find(c => c.isDigit).map(c => c.toString)
     val lastDigit = str.reverse.find(c => c.isDigit).map(c => c.toString)
 
     (firstDigit.get + lastDigit.get).toInt
-}
 
-def convertFirstAndLastStringToNumber(str: String) = {
+  def convertFirstAndLastStringToNumber(str: String): String =
     val indexedNumbers = numbers.zipWithIndex.map(n => (n._1, n._2 + 1))
-    val firstPart = indexedNumbers.map(n => NumberPointer(n._1, n._2, str.indexOf(n._1)))
-                                  .filter { _.index != -1 }
-                                  .minByOption { _.index }
-                                  .map(p => str.patch(p.index, p.value.toString, 0))
-                                  .getOrElse { str }
+    val firstPart = indexedNumbers
+      .map(n => NumberPointer(n._1, n._2, str.indexOf(n._1)))
+      .filter(_.index != -1)
+      .minByOption(_.index)
+      .map(p => str.patch(p.index, p.value.toString, 0))
+      .getOrElse(str)
 
-    val targetPart = indexedNumbers.map(n => NumberPointer(n._1, n._2, firstPart.lastIndexOf(n._1)))
-                                  .filter { _.index != -1 }
-                                  .maxByOption { _.index }
-                                  .map(p => firstPart.patch(p.index, p.value.toString, 0))
-                                  .getOrElse { firstPart }
+    indexedNumbers
+      .map(n => NumberPointer(n._1, n._2, firstPart.lastIndexOf(n._1)))
+      .filter(_.index != -1)
+      .maxByOption(_.index)
+      .map(p => firstPart.patch(p.index, p.value.toString, 0))
+      .getOrElse(firstPart)
 
-    joinFirstAndLastDigit(targetPart)
-}
+  @main def day1Solution: Unit =
+    Using.Manager { use =>
 
-@main def day1Solution: Unit = {
-  Using.Manager { use =>
+      val calibrationData = use(Source.fromResource("day1/input.txt")).getLines().toSeq
+      val calibrationValuesSum = calibrationData
+        .map(joinFirstAndLastDigit)
+        .sum
+      println(f"Calibration value sum: ${calibrationValuesSum}")
 
-    val inputBufferedSource = use(Source.fromResource("day1/input.txt"))
-    val sum = inputBufferedSource.getLines()
-      .map(convertFirstAndLastStringToNumber)
-      .sum
+      val properCalibrationValuesSum = calibrationData
+        .map(calibrationValue => {
+          val parsedCalValue = convertFirstAndLastStringToNumber(calibrationValue)
+          joinFirstAndLastDigit(parsedCalValue)
+        })
+        .sum
+      println(f"Proper calibration value sum: ${properCalibrationValuesSum}")
+    }
 
-    print("\nResult is\n")
-    print(sum)
-    print("\n")
-  }
-}
-  
+end Day1Challenge
