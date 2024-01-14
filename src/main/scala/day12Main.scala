2bb0f9110c289c71b50799c835f01eb976ec9719
Bojan Makljenovic <maklja90@gmail.com>
2024-01-14 21:34:16 +0100

    Next

--- src/main/scala/day12Main.scala
+++ src/main/scala/day12Main.scala
@@ -37,164 +37,6 @@ object Day12Challenge:
       SpringRow(springs, count)
     })
 
-  def splitSpringRowIntoRanges(springRows: Seq[SpringRow]) =
-
-    @tailrec
-    def splitSpringRow(
-        springsStatus: String,
-        results: Seq[SpringRowRange] = Seq()
-    ): Seq[SpringRowRange] =
-      val remainingSpringsStatus = springsStatus.dropWhile(spring => spring == SpringStatus.Operational.getValue())
-      if (remainingSpringsStatus.isEmpty()) {
-        return results
-      }
-
-      val springRange = remainingSpringsStatus.takeWhile(spring => spring != SpringStatus.Operational.getValue())
-      val range = SpringRowRange(springRange)
-      splitSpringRow(
-        remainingSpringsStatus.slice(springRange.length(), remainingSpringsStatus.length()),
-        results :+ range
-      )
-
-    springRows.map(springRow => (springRow, splitSpringRow(springRow.springsStatus)))
-
-  @tailrec
-  def markSpringStatusCount(
-      springStatusCount: Int,
-      range: SpringRowRange,
-      markPosition: Int = 0
-  ): Option[(Int, SpringRowRange)] =
-    val availableSpots = range.springStatusChunk.slice(markPosition, range.springStatusChunk.length())
-    if (availableSpots.length() < springStatusCount) {
-      return None
-    }
-
-    val markRange = Range(markPosition, Math.min(range.springStatusChunk.length(), markPosition + springStatusCount))
-    val markedSpots =
-      markRange
-        .foldLeft(range.springStatusChunk)((springStatus, i) => {
-          if (springStatus(i) == SpringStatus.Malfunction.getValue()) {
-            springStatus
-          } else {
-            springStatus.patch(i, SpringStatus.Marked.getValueStr(), 1)
-          }
-        })
-
-    val countCheck = markedSpots
-      .slice(markRange.head, markRange.head + markRange.length + 1)
-      .count(status => status != SpringStatus.Unknown.getValue())
-    if (countCheck == springStatusCount) {
-      val maxRange =
-        markedSpots.zipWithIndex
-          .slice(markRange.head, markRange.head + markRange.length)
-          .find(_._1 == SpringStatus.Malfunction.getValue())
-          .map(_._2 + springStatusCount)
-          .getOrElse(range.springStatusChunk.length())
-      val nextSpringRange =
-        SpringRange(springStatusCount, Range(markRange.head, maxRange))
-      val springRowRanges = range.ranges.reverse
-        .foldLeft(Seq(nextSpringRange))((newRanges, curRange) => {
-          val updatedMaxRange = Math.min(curRange.range.end, newRanges.last.range.last - 1)
-          val updatedCurRange = Range(curRange.range(0), updatedMaxRange)
-          newRanges :+ curRange.copy(range = updatedCurRange)
-        })
-        .reverse
-
-      Some(
-        markPosition + markRange.length + 1,
-        SpringRowRange(
-          markedSpots,
-          springRowRanges
-        )
-      )
-    } else {
-      markSpringStatusCount(springStatusCount, range, markPosition + 1)
-    }
-
-  def markSpringRowRanges(springStatusCount: Seq[Int], springRowRanges: Seq[SpringRowRange]): Seq[SpringRowRange] =
-
-    @tailrec
-    def markNextSpringRows(
-        springStatusCount: Seq[Int],
-        ranges: Seq[SpringRowRange],
-        markPosition: Int = 0,
-        markedSpringRowRanges: Seq[SpringRowRange] = Seq()
-    ): Seq[SpringRowRange] =
-      if (springStatusCount.isEmpty) {
-        return markedSpringRowRanges ++ ranges
-      }
-
-      if (ranges.isEmpty) {
-        throw new RuntimeException(f"Invalid spring row ranges for sequence ${springStatusCount}")
-      }
-
-      val springRowRange = ranges.head
-      val statusCount = springStatusCount.head
-
-      markSpringStatusCount(statusCount, springRowRange, markPosition) match
-        case None =>
-          markNextSpringRows(springStatusCount, ranges.tail, 0, markedSpringRowRanges :+ springRowRange)
-        case Some((nextMarkPosition, newSpringStatusRange)) =>
-          markNextSpringRows(
-            springStatusCount.tail,
-            newSpringStatusRange +: ranges.tail,
-            nextMarkPosition,
-            markedSpringRowRanges
-          )
-
-    markNextSpringRows(springStatusCount, springRowRanges)
-
-  def combinations(springStatusChunk: String) =
-    val shiftToRightCount = springStatusChunk
-
-    // *#??* => #*?*
-    @tailrec
-    def combinationsHelper(springStatus: Option[Char], restLeftStatus: String, result: String = ""): String =
-      springStatus match
-        case Some(status) =>
-          if (status == SpringStatus.Malfunction.getValue()) {
-            return combinationsHelper(restLeftStatus.headOption, restLeftStatus.tail, result :+ status)
-          }
-
-          val nextStatusOption = restLeftStatus.headOption
-          nextStatusOption match {
-            case Some(nextStatus)
-                if nextStatus == SpringStatus.Malfunction.getValue() || nextStatus == SpringStatus.Marked.getValue() =>
-              combinationsHelper(springStatus, restLeftStatus.tail, result :+ nextStatus)
-            case Some(nextStatus) if nextStatus == SpringStatus.Unknown.getValue() =>
-              combinationsHelper(springStatus, restLeftStatus.tail, result :+ nextStatus)
-            case _ =>
-              combinationsHelper(restLeftStatus.tail.headOption, restLeftStatus.tail.drop(1), result :+ status)
-            // val skipStatus =
-            //   restLeftStatus.tail.takeWhile(springStatus => springStatus != SpringStatus.Marked.getValue())
-            // val newResult = (result :+ status) + skipStatus
-            // val remainingStatus = restLeftStatus.tail.slice(skipStatus.length(), restLeftStatus.tail.length())
-            // combinationsHelper(remainingStatus.headOption, remainingStatus.tail, newResult)
-          }
-        // if (nextStatus == SpringStatus.Marked.getValue()) {
-        //   combinationsHelper(springStatus, restLeftStatus.tail, result :+ nextStatus)
-        // }
-        case _ =>
-          result
-
-    if (springStatusChunk.contains(SpringStatus.Unknown.getValueStr())) {
-      combinationsHelper(springStatusChunk.headOption, springStatusChunk.tail)
-    } else {
-      springStatusChunk
-    }
-
-  def xx(springRowRange: SpringRowRange) =
-    val springRanges = springRowRange.ranges
-    val springStatusRowChunk = springRowRange.springStatusChunk
-    val chunks = springRanges.reverse.map(springRange =>
-      springStatusRowChunk.slice(springRange.range.start, springRange.range.end)
-    )
-
-    val tt = chunks.foreach(t => {
-      println(f"--->${t}")
-      println(combinations(t))
-    })
-
   @tailrec
   def findMalfunctions(springRowStatus: String, ranges: Seq[Range] = Seq()): Seq[Range] = {
     val malfunctionIdx = springRowStatus.indexOf(SpringStatus.Malfunction.getValueStr())
@@ -209,8 +51,10 @@ object Day12Challenge:
       )
     }
   }
-// here
-  def malfunctionRanges(springRowStatus: String): Seq[Range] = {
+
+  type RangeMatch = (Int, Option[Range])
+
+  def findMalfunctionRanges(springRowStatus: String): Seq[Range] = {
 
     @tailrec
     def malfunctionRangesHelper(
@@ -251,17 +95,133 @@ object Day12Challenge:
     malfunctionRangesHelper(springRowStatus.zipWithIndex)
   }
 
-  def bestMatch(springRow: SpringRow) = {
-    println(malfunctionRanges(springRow.springsStatus))
+  def bestMatchRange(springsStatus: String, springsCount: Seq[Int], malfunctionRange: Range) = {
+    val malfunctionChunk = springsStatus.slice(malfunctionRange.start, malfunctionRange.end)
+    val leftSideFields =
+      springsStatus
+        .slice(0, malfunctionRange.start)
+        .reverse
+        .takeWhile(_ != SpringStatus.Operational.getValue())
+        .length()
+    val rightSideFields =
+      springsStatus
+        .slice(malfunctionRange.end, springsStatus.length())
+        .takeWhile(_ != SpringStatus.Operational.getValue())
+        .length()
+
+    def checkBestMatch(
+        springStatusCounts: Seq[Int],
+        results: Seq[RangeMatch] = Seq()
+    ): Seq[RangeMatch] = {
+      if (springStatusCounts.isEmpty) {
+        return results
+      }
+
+      val sprintStatusCount = springStatusCounts.head
+      if (malfunctionChunk.length() > sprintStatusCount) {
+        return checkBestMatch(springStatusCounts.tail, results :+ (sprintStatusCount, None))
+      }
+
+      val missingFields = sprintStatusCount - malfunctionChunk.length()
+      val maxLeft = Math.min(leftSideFields, missingFields)
+      val maxRight = Math.min(rightSideFields, missingFields)
+      val n = malfunctionChunk.length() + maxLeft + maxRight
+      if (n < sprintStatusCount) {
+        return checkBestMatch(springStatusCounts.tail, results :+ (sprintStatusCount, None))
+      }
+
+      val rangeStart = malfunctionRange.start - maxLeft
+      val rangeEnd = malfunctionRange.end + maxRight
+
+      return results :+ (sprintStatusCount, Some(Range(rangeStart, rangeEnd)))
+    }
+
+    checkBestMatch(springsCount)
+  }
+
+  def lastPartCheck(springRow: SpringRow, lastRangeMatch: RangeMatch) = {
+    val remainingSpringRow = springRow.springsStatus.drop(lastRangeMatch._2.get.start + lastRangeMatch._1)
+
+    def xxxxx(springsStatus: String, sprintCount: Seq[Int]): Seq[Int] = {
+      if (springsStatus.isEmpty() || sprintCount.isEmpty) {
+        return sprintCount
+      }
+
+      val operationalFields = springsStatus.takeWhile(_ == SpringStatus.Operational.getValue())
+      val springStatus =
+        springsStatus.drop(operationalFields.length()).takeWhile(_ != SpringStatus.Operational.getValue())
+
+      val fieldsRequired = if (operationalFields.isEmpty()) sprintCount.head + 1 else sprintCount.head
+      if (fieldsRequired <= springStatus.length()) {
+        xxxxx(springsStatus.drop(operationalFields.length() + fieldsRequired), sprintCount.tail)
+      } else {
+        xxxxx(springsStatus.drop(operationalFields.length() + springStatus.length()), sprintCount)
+      }
+      // ##?.?.????? 3, 2, 1
+    }
+
+    xxxxx(remainingSpringRow, springRow.springsCount)
+  }
+
+  def xxx(
+      springRow: SpringRow,
+      malfunctionRanges: Seq[Range],
+      rangeMatches: Seq[RangeMatch] = Seq()
+  ): (Boolean, Seq[RangeMatch]) = {
+    if (malfunctionRanges.isEmpty) {
+      return rangeMatches.lastOption
+        .map(range => {
+          val matched = lastPartCheck(springRow, range).isEmpty
+          (matched, rangeMatches ++ springRow.springsCount.map((_, None)))
+        })
+        .getOrElse((true, springRow.springsCount.map((_, None))))
+    }
+
+    val malfunctionRange = malfunctionRanges.head
+    val lastRange = rangeMatches.lastOption.flatMap(_._2)
+    val isTaken =
+      lastRange.map(!_.intersect(malfunctionRange).isEmpty).getOrElse(false)
+
+    if (isTaken) {
+      return xxx(springRow, malfunctionRanges.tail, rangeMatches)
+    }
+
+    val bestRangeMatch =
+      bestMatchRange(springRow.springsStatus, springRow.springsCount, malfunctionRange)
+
+    val result = xxx(
+      springRow.copy(springsCount = springRow.springsCount.drop(bestRangeMatch.length)),
+      malfunctionRanges.tail,
+      rangeMatches ++ bestRangeMatch
+    )
+
+    if (result._1) {
+      result
+    } else {
+      xxx(
+        springRow.copy(springsCount = springRow.springsCount.drop(1)),
+        malfunctionRanges,
+        rangeMatches :+ (springRow.springsCount.head, None)
+      )
+    }
+  }
+
+  def bestMatch(springRow: SpringRow): (Boolean, Seq[RangeMatch]) = {
+    val malfunctionRanges = findMalfunctionRanges(springRow.springsStatus)
+    val results = xxx(springRow, malfunctionRanges)
+
+    results
   }
 
   @main def day12Main(): Unit =
     Using.Manager { use =>
       try {
         val springsData =
-          use(Source.fromResource("day12/smallInput.txt")).getLines().toSeq
+          use(Source.fromResource("day12/input.txt")).getLines().toSeq
         val springRows = parseSpringData(springsData)
-        springRows.foreach(bestMatch(_))
+        springRows.foreach(x => {
+          println(f"${x} => ${bestMatch(x)}")
+        })
         // val markedSpringRowRanges = splitSpringRowIntoRanges(springRows)
         //   .map((springRowRangeTuple) => {
         //     val (springRow, springROwRanges) = springRowRangeTuple
@@ -277,35 +237,4 @@ object Day12Challenge:
         case e: RuntimeException => e.printStackTrace()
       }
     }
-
-  // *?*??
-  // [0,2],[2,4]
-  // 0 + 1 not in [2,4] => 3
-  // 1 + 1 is in [2,4] => new range [3,4] => 2
-  // 2 + 1 is in [2,4] => new range [4,4] => 1
-
-  // *?*???
-  // [0,3], [2,5]
-  // 0 + 1 not [2,5] => 4
-  // 1 + 1 is [2,5] => new range [3,5] => 3
-  // 2 + 1 is [2,5] => new range [4,5] => 2
-  // 3 + 1 is [2,5] => new range [5,5] => 1
-
-  // *??*??*??
-  // [0, 4], [3,6], [6, 8]
-  // 0 + 1 not in [3,6] => 4 + 3 = 7
-  // 1 + 1 not in [3,6] => 4 + 3 = 7
-  // 2 + 1 is [3,6] => new range [4,6] => 3 + 3 = 6
-  // 3 + 1 is [3,6] => new range [5,6] => 2 + 3 = 5
-  // 4 + 1 is [3,6] => new range [6,6] is in range [6,8] => [6,6][7,8] => 1 + 2 = 3
-
-  // *?*#?? (1, 2)
-  // [0,1], [2,4] => 1, 2
-  // 0 + 1 not in [2,4] =>  2
-  // 1 + 1 in [2,4] => new range [3,4] => 1
-
-  // *?*##? (1, 3)
-  // [0,1], [2,5] => 2, 2
-  // 0 + 1 not in [2,5] => 2
-  // 1 + 1 in [2,5] => new range [3,5] => 1
 end Day12Challenge
